2bea161e2847b0bb4b867fd7fb530ea1d616c668
cityo1 <ansrnrgus1@naver.com>
2026-01-20 17:04:40 +0900

    hooks init

---
+++ react_hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline">React Hooks</h2>
+    </>
+  );
+};
+
+export default App;
