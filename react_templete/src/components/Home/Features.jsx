33d4a53d994d2dc7a86931f535676ad17071f1cf
cityo1 <ansrnrgus1@naver.com>
2026-01-22 17:11:26 +0900

    features in progress

---
+++ react_templete/src/components/Home/Features.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>
+      <div>
+        <span className='text-indigo-500'>Features</span>
+        <h2>Easily Build <span className='bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text'>Your Codes</span></h2>
+      </div>
+    </div>
+  )
+}
+
+export default Features
